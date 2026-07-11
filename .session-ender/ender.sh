5a9437787c780c76a8c17975a683623acc1664e8
zerauj-kcire <zeraujkcire@gmail.com>
2026-07-11 12:55:04 -0600

    update the ender

--- .session-ender/ender.sh
+++ .session-ender/ender.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # update the solved problems
-grep -o "adventofcode.*" $HOME/.local/share/lf/tags | sort -n | tree --fromfile > README.md;
+grep -o "adventofcode.*" $HOME/.local/share/lf/tags | sort -n | tree --fromfile > README.txt;
 # do the git stuff
 git add .
-read -s -p "Message to commit: " msg;
+read -p "Message to commit: " msg;
 git commit -m "$msg";
-cat $HOME/.config/token-git.txt | tr -d "\n" | wl-copy;
+cat $HOME/.config/token.git.txt | tr -d "\n" | wl-copy;
 git push origin master;
